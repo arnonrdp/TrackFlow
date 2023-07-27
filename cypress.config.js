ffbe03d4323e1ed6b73b49a4d34e01bf3e399972
Arnon <arnonrdp@gmail.com>
2023-07-27 20:55:50 -0300

    Saving videos

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  videosFolder: 'cypress/videos',
+  video: true,
   e2e: {
     baseUrl: 'http://localhost:5173'
   }
